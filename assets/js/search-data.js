a28db182843cb7bad24b5999d337d61bdbc0645a
maltefranke <65611948+maltefranke@users.noreply.github.com>
2025-08-16 15:00:50 +0000

    Deploying to gh-pages from @ maltefranke/maltefranke.github.io@9806799ab30374d62bf4e4fdf15c6b6d67402a9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,16 +384,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-have-successfully-defended-my-master-s-thesis-at-mit",
+          title: 'I have successfully defended my Master’s thesis at MIT!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-have-started-my-internship-at-dunia-in-berlin",
+          title: 'I have started my internship at Dunia in Berlin!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
