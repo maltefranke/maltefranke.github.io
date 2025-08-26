ca4fc3324a843976027df6dd9c48c7795046b301
maltefranke <65611948+maltefranke@users.noreply.github.com>
2025-08-26 19:55:15 +0000

    Deploying to gh-pages from @ maltefranke/maltefranke.github.io@3b0bf930dcec056ba7bb0a10175ff5166b81a720 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,35 +440,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%6D%61%6C%74%65.%66%72%61%6E%6B%65@%69%6E%66.%65%74%68%7A.%63%68", "_blank");
         },
       },{
       id: 'light-theme',
